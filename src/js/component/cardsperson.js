f25433101498c58bd4b592dbce200d49e4d03522
sunb33 <behb233@gmail.com>
2020-08-13 17:06:42 +0000

    cards

---
+++ src/js/component/cardsperson.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export function Card() {
+	return (
+		<div class="card" style="width: 18rem;">
+			<img src="..." class="card-img-top" alt="..." />
+			<div class="card-body">
+				<h5 class="card-title"> TITLE </h5>
+				<p class="card-text">DESCRIPTION</p>
+				<a href="#" class="btn btn-primary">
+					MORE INFORMATION
+				</a>
+			</div>
+		</div>
+	);
+}
+
+ReactDOM.render(<Card />), document.querySelector("#app");
